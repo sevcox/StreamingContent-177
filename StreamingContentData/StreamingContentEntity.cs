fee08c3ef52415b0fb9b4238040eadc9f2e354b0
Severa Cox <66082969+sevcox@users.noreply.github.com>
2023-11-08 21:29:07 -0500

    first commit

---
+++ StreamingContentData/StreamingContentEntity.cs
@@ -0,0 +1,19 @@
+﻿namespace StreamingContentEntityData;
+
+// application - user can keep an up to date list of streaming content
+// there are several different kinds of streaming content - movies, tv shows, etc.
+// collection of streaming content objects
+// repository pattern architecture - data and business logic seperate
+
+//inheritance with Streaming Content objects
+public abstract class StreamingContentEntity
+{
+    public StreamingContentEntity() {}
+
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public double StarRating { get; set; }
+    public double Duration { get; set; }
+    public string Location { get; set; }
+    
+}
